4ec059d86c9b3e33f5c60849866e960515c06a2e
leilaotieno2 <leila.otieno@student.moringaschool.com>
2023-04-06 18:11:25 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(namesArray, event) {
+  const thankYouMessages = [];
+  for (let i = 0; i < namesArray.length; i++) {
+    thankYouMessages.push(`Thank you, ${namesArray[i]}, for the wonderful ${event} gift!`);
+  }
+  return thankYouMessages;
+}
+
+function countDown(number) {
+  while (number >= 0) {
+    console.log(number);
+    number--;
+  }
+}
+
